6ac721abc0f8e0c126be67aecd3c672028307efd
jude okoroafor <judeokoroafor@gmail.com>
2020-07-02 17:32:54 +0100

    Completely new design
    Had to change the design

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,46 @@
-
-// TO HIDE MOBILE MENU ON CLICK
-$('.navbar-nav li a').on('click', function(){
-    if(!$( this ).hasClass('dropdown-toggle')){
-        $('.navbar-collapse').collapse('hide');
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+    /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 5;
+    var totalItems = $('.carousel-item').length;
+ 
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
     }
 });
+
+
+// -------------------
+
+
+$(".project-detail").slick({
+	slidesToShow: 1,
+	arrows: false,
+	asNavFor: '.project-strip',
+	autoplay: true,
+	autoplaySpeed: 3000
+});
+
+$(".project-strip").slick({
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	arrows: false,
+	asNavFor: '.project-detail',
+	dots: false,
+	infinite: true,
+	centerMode: true,
+	focusOnSelect: true
+});
+//# sourceURL=pen.js
